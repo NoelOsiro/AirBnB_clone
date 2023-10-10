613db81a6a9c8e5d2d67f1cef00d19676ce472c0
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-10-10 13:16:02 +0300

    Create base_model_test.py

---
+++ tests/base_model_test.py
@@ -0,0 +1,69 @@
+import unittest
+import pycodestyle
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    def test_pep8_conformance(self):
+        """
+        Test that the code complies with PEP 8.
+        """
+        style = pycodestyle.StyleGuide()
+        result = style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0, "PEP 8 style issues found")
+
+    def test_base_model_attributes(self):
+        """
+        Test the attributes of the BaseModel instance.
+        """
+        model = BaseModel()
+        self.assertTrue(hasattr(model, 'id'))
+        self.assertTrue(hasattr(model, 'created_at'))
+        self.assertTrue(hasattr(model, 'updated_at'))
+
+    def test_base_model_id(self):
+        """
+        Test the uniqueness of the 'id' attribute.
+        """
+        model1 = BaseModel()
+        model2 = BaseModel()
+        self.assertNotEqual(model1.id, model2.id)
+
+    def test_base_model_created_at_updated_at(self):
+        """
+        Test the 'created_at' and 'updated_at' attributes.
+        """
+        model = BaseModel()
+        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
+
+    def test_base_model_save(self):
+        """
+        Test the 'save' method.
+        """
+        model = BaseModel()
+        prev_updated_at = model.updated_at
+        model.save()
+        self.assertNotEqual(prev_updated_at, model.updated_at)
+
+    def test_base_model_to_dict(self):
+        """
+        Test the 'to_dict' method.
+        """
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['created_at'], model.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'], model.updated_at.isoformat())
+
+    def test_base_model_str(self):
+        """
+        Test the '__str__' method.
+        """
+        model = BaseModel()
+        expected_str = "[BaseModel] ({}) {}".format(model.id, model.__dict__)
+        self.assertEqual(str(model), expected_str)
+
+
+if __name__ == '__main__':
+    unittest.main()
